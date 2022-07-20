38cbe8cc33fabf039e911f5eb455731e17f5729c
iTzNeMeSiS <codingdre@gmail.com>
2022-07-20 00:12:38 -0400

    Fixed login view/ logged out view

--- apps/foundations/views.py
+++ apps/foundations/views.py
@@ -7,6 +7,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-@login_required(login_url="/login/")
+# @login_required(login_url="/login/")
 def foundations(request):
     return render(request, 'foundations.html')
--- apps/paths/views.py
+++ apps/paths/views.py
@@ -8,6 +8,6 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required(login_url="/login/")
+# @login_required(login_url="/login/")
 def paths(request):
     return render(request, 'paths.html')
